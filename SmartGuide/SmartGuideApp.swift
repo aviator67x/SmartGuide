caf3a768a69ac480b6934445a23dc32c966c3163
Andrew Kasilov <aviator67x@gmail.com>
2024-06-06 10:42:44 +0200

    initial

---
+++ SmartGuide/SmartGuideApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartGuideApp.swift
+//  SmartGuide
+//
+//  Created by Andrew Kasilov on 06.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct SmartGuideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
